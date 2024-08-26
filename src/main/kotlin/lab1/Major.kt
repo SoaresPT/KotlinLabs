3f144979dbd2e0e87c9a0bd1ee6cfddb1e774a98
SoaresPT <soares_123@msn.com>
2024-08-26 10:20:17 +0300

    Fix weighted avg

--- src/main/kotlin/lab1/Major.kt
+++ src/main/kotlin/lab1/Major.kt
@@ -8,17 +8,19 @@ class Major (val name: String) {
     }
 
     fun stats(): Triple<Double, Double, Double> {
+
         var majorMin = Double.MAX_VALUE
         var majorMax = Double.MIN_VALUE
-        var majorAverage = 0.0
-        var totalGrades = 0.0
-        var totalStudents = 0
+        var totalAverage = 0.0
+
         for (student in students) {
-            majorMin = minOf(majorAverage, student.minMaxGrades().first)
-            majorMax = maxOf(majorAverage, student.minMaxGrades().second)
-            majorAverage = student.weightedAverage() / totalStudents
-            totalStudents++
+            val studentAverage = student.weightedAverage()
+            majorMin = minOf(majorMin, studentAverage)
+            majorMax = maxOf(majorMax, studentAverage)
+            totalAverage += studentAverage
         }
+
+        val majorAverage = totalAverage / students.size
 
         return Triple(majorMin, majorMax, majorAverage)
     }
@@ -39,6 +41,7 @@ class Major (val name: String) {
                 }
             }
         }
+
         val avgGrade = if (totalStudents > 0) totalGrades / totalStudents else 0.0
 
         return Triple(minGrade, maxGrade, avgGrade)
